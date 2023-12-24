4689e1ea03d70333cbcb600272b61d5362792e5b
Emre YÜKSEL <equesse78@gmail.com>
2023-12-24 19:16:25 +0300

    a

---
+++ DevFramework.core/DataAcces/NHibarnate/NhQueryableRepository.cs
@@ -0,0 +1,40 @@
+﻿using DevFramework.Core.Entities;
+using NHibernate.Persister.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevFramework.Core.DataAcces.NHibarnate
+{
+    public class NhQueryableRepository<T>:IQueryableRepository<T> where T : class,IEntity,new()
+    {
+
+        private NHibernateHelper _nHibarnateHelper;
+        private IQueryable<T> _entities;
+        
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibarnateHelper = nHibernateHelper;
+        }
+
+        public IQueryable<T> Table
+        {
+            get { return this.Entities; }
+        }
+
+        // get { return _entities ?? (_entities = _nHibernateHelper.OpenSession().Query<T>());
+        public virtual IQueryable<T> Entities
+        {
+            get
+            {
+                {
+                    return _entities ?? (_entities = _nHibarnateHelper.OpenSession().Query<T>());
+                }
+            }
+        }
+
+        // entities null ise yeni açıyor değil ise entities döndürüyor
+        IQueryableRepository<T> IQueryableRepository<T>.Table => throw new NotImplementedException();
+    }
+}
