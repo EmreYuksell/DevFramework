4b126bd57707d0bcd6ff47bb18d71b4a1cee3b47
Emre YÜKSEL <equesse78@gmail.com>
2024-02-05 14:39:30 +0300

    a

--- DevFramework.Northwind.DataAcces/Concrete/NHibernate/Helpers/SqlServerHelper.cs
+++ DevFramework.Northwind.DataAcces/Concrete/NHibernate/Helpers/SqlServerHelper.cs
@@ -15,8 +15,9 @@ namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helpers
         protected override ISessionFactory InitializeFactory()
         {
             return Fluently.Configure().Database(MsSqlConfiguration.MsSql2012
-            .ConnectionString(c => c.FromConnectionStringWithKey("NorthwindContext")))
-            .Mappings(t => t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly())).BuildSessionFactory();
+            .ConnectionString(c => c.ConnectionString("NorthwindContext")))
+            .Mappings(t => t.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
+            .BuildSessionFactory();
 
         }
     }
